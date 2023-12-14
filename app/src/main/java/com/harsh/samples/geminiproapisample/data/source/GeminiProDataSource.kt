dab59ca25b1e81747a99fe87ba5d6abdcdfc4a8c
Harsh Nandwani <nandwaniharsh@gmail.com>
2023-12-14 23:24:45 +0530

    Created Gemini Pro data source

---
+++ app/src/main/java/com/harsh/samples/geminiproapisample/data/source/GeminiProDataSource.kt
@@ -0,0 +1,34 @@
+package com.harsh.samples.geminiproapisample.data.source
+
+import com.google.ai.client.generativeai.GenerativeModel
+
+class GeminiProDataSource(
+    private val apiKey: String
+) {
+
+    private val geminiProModel = GenerativeModel(
+        modelName = "gemini-pro",
+        apiKey = apiKey
+    )
+
+    private val chat = geminiProModel.startChat()
+
+    suspend fun generateIndividualResponse(prompt: String): String {
+        return try {
+            val response = geminiProModel.generateContent(prompt).text
+            response ?: ""
+        } catch (e: Exception) {
+            e.localizedMessage ?: "Unable to generate response"
+        }
+    }
+
+    suspend fun generateOngoingChatResponse(message: String): String {
+        return try {
+            val response = chat.sendMessage(message).text
+            response ?: ""
+        } catch (e: Exception) {
+            e.localizedMessage ?: "Unable to generate response"
+        }
+    }
+
+}
