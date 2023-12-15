f2c2ecda39072b00b747a76ca7a60d9b2c80d1f6
Harsh Nandwani <nandwaniharsh@gmail.com>
2023-12-15 11:08:20 +0530

    Application class for hilt

---
+++ app/src/main/java/com/harsh/samples/geminiproapisample/GeminiProApiSampleApp.kt
@@ -0,0 +1,7 @@
+package com.harsh.samples.geminiproapisample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GeminiProApiSampleApp: Application()
